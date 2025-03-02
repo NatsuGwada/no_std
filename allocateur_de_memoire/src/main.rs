adce4798ba257e60413b72c11ef7cbbfa085c112
Sanndy MANIJEAN <145344071+NatsuGwada@users.noreply.github.com>
2025-03-02 20:43:49 +0100

    Update main.rs

--- allocateur_de_memoire/src/main.rs
+++ allocateur_de_memoire/src/main.rs
@@ -1,4 +1,23 @@
-/// Programme Principal
-fn main() {
-    println!("Hello, world!");
+//  Directive(attribut) donnée à rust pour Désactiver la Bibliothèque Standard d'inpout et D'output
+#![no_std]
+
+// Structure qui contient les information sur la panic du kernel
+use core::panic::PanicInfo;
+
+
+#[panic_handler]   // Directive donné à  rust pour utilisé  la fonction lorsqu'un panic se produit
+// Fonction qui va ignorer les informations de panic du kernel
+fn panic(_info: &PanicInfo) -> !{
+
+    if let Some(location) = _info.location() {
+        let file = location.file();
+        let line = location.line();
+
+    }
+    
+    loop{}
+
+
 }
+
+
