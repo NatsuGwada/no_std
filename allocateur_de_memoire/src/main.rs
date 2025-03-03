724bacd6fe8a7856e927b41aa1e41838de07c770
Sanndy MANIJEAN <145344071+NatsuGwada@users.noreply.github.com>
2025-03-03 21:49:33 +0100

    Update main.rs

--- allocateur_de_memoire/src/main.rs
+++ allocateur_de_memoire/src/main.rs
@@ -2,9 +2,9 @@
 #![no_std]
 
 // Structure qui contient les information sur la panic du kernel
-use core::panic::PanicInfo;
-use core::alloc::{GlobalAlloc, Layout};
-use core::ptr;
+use core::panic::PanicInfo;  // depuis la crate "core", on importe PanicInfo
+use core::alloc::{GlobalAlloc, Layout}; // importation de Global et Layout depuis core::Alloc (pour la taille et l'alignement des allocations)
+use core::ptr; // ça c'est pour manipuler les pointeurs a bas niveau
 
 // Fonction qui va ignorer les informations de panic du kernel
 fn panic(_info: &PanicInfo) -> !{
@@ -17,12 +17,23 @@ fn panic(_info: &PanicInfo) -> !{
 pub struct SlabAllocator {
 
     // mémoire utilisé pour l'allocation
-    memory_start: usize,
-    memory_size: usize,
+    memory_start: usize,  // adresse de debut de la zone mémoire
+    memory_size: usize,   //taille de la zone mémoire à gerer
 
     // pointeur vers la prochaine zone mémoire libre
     next_free: usize,
 
 }
 
-// 
+impl SlabAllocator{
+    // constructeur pour initialisé l'allocateur
+    pub const fn new(start: usize, size: usize) -> SlabAllocator {
+        SlabAllocator{
+            memory_start: start,
+            memory_size: size,
+            next_free: start,
+        }
+    }
+}
+
+// Imprémentation du trait GlobalAlloc
