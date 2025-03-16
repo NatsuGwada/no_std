c757f488ccaba15c5ebf7c7d3ff64273aad6ca42
Sanndy MANIJEAN <145344071+NatsuGwada@users.noreply.github.com>
2025-03-16 14:31:00 +0100

    Update main.rs

--- allocateur_de_memoire/src/main.rs
+++ allocateur_de_memoire/src/main.rs
@@ -1,70 +1,30 @@
-//  Directive(attribut) donnée à rust pour Désactiver la Bibliothèque Standard d'inpout et D'output
 #![no_std]
+#![no_main]
 
-// Structure qui contient les information sur la panic du kernel
-use core::panic::PanicInfo;  // depuis la crate "core", on importe PanicInfo
-use core::alloc::{GlobalAlloc, Layout}; // importation de Global et Layout depuis core::Alloc (pour la taille et l'alignement des allocations)
-use core::ptr; // ça c'est pour manipuler les pointeurs a bas niveau
+extern crate slab_allocator;
 
-// Fonction qui va ignorer les informations de panic du kernel
-fn panic(_info: &PanicInfo) -> !{
-
-    loop{}
-}
+use core::panic::PanicInfo;  // information sur les kernel panik
+use slab_allocator::init_global_allocator;  //initialise le global allocator pour utilisé mon slab
 
 
-// structure pour l'allocateur slab
-pub struct SlabAllocator {
+#[repr(align(4096))]
+struct MemoryRegion([u8; 1024 * 1024]); // 1MB
 
-    // mémoire utilisé pour l'allocation
-    memory_start: usize,  // adresse de debut de la zone mémoire
-    memory_size: usize,   //taille de la zone mémoire à gerer
+static mut MEMORY: MemoryRegion = MemoryRegion([0; 1024 * 1024]);
 
-    // pointeur vers la prochaine zone mémoire libre
-    next_free: usize,
-
-}
-
-impl SlabAllocator{
-    // constructeur pour initialisé l'allocateur
-    pub const fn new(start: usize, size: usize) -> SlabAllocator {
-        SlabAllocator{
-            memory_start: start,
-            memory_size: size,
-            next_free: start,
-        }
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Initialiser l'allocateur avec une région de mémoire hypothétique
+    unsafe {
+        let addr = &mut MEMORY.0 as *mut u8 as usize;
+        init_global_allocator(addr, 1024 * 1024);
     }
+    
+    // Juste une boucle pour que le programme ne se termine pas
+    loop {}
 }
 
-// Imprémentation du trait GlobalAlloc
-unsafe  impl GlobalAlloc for SlabAllocator {
-    unsafe fn alloc(&self, layout: Layout) -> mut u8{
-        // vérification de si on à asser de mémoire
-        let size = layout.size();
-        let aling = layout.align();
-
-        //alignement de la mémoire
-        let mut current = self.next_free;
-        let remainder = current % align;
-        if remainder != 0 {
-            current += align - remainder
-        }
-
-        // Vérification qu'on à asser d'espace en mémoire
-        if current + size > self.memory_start + self.memory_size {
-            return ptr::null_mut();
-        }
-
-        // Return un pointeur vers la mémoire allouée
-        current as *mut u8
-    }    
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        
-    }
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
-
-// pour que l'allocateur soit utilisable par le systeme
-#[global_allocator]
-static ALLOCATOR: SlabAllocator = SlabAllocator::new(0x10000, 0x10000);
-
